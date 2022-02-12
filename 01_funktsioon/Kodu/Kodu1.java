019ab483ba8705dccdb7eb4faed4c41337b0ad6d
Egon Ubina <egubin@tlu.ee>
2022-02-12 13:51:13 +0200

    Test et Github Desktop tööle saada. Kodutöö teen homme.

---
+++ 01_funktsioon/Kodu/Kodu1.java
@@ -0,0 +1,10 @@
+public class Kodu1{
+	public static void main(String[] arg){
+		int arv1=Integer.parseInt(arg[0]);
+		int arv2=Integer.parseInt(arg[1]);
+		System.out.println(arv1*arv2);
+		for(int i=0; i<arv2; i++){
+			System.out.println("Tere.");
+		}
+	}
+}
