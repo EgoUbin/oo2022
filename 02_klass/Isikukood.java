0b0bf3dcbe4ee789526815600ce21fd0f74c223b
Egon Ubina <egubin@tlu.ee>
2022-02-14 13:45:58 +0200

    Põrgu

---
+++ 02_klass/Isikukood.java
@@ -0,0 +1,13 @@
+public class Isikukood{
+	private String kood;
+	public Isikukood(String uusKood){kood=uusKood;}
+	public String sugu(){
+			return(Integer.parseInt(kood.substring(0,1)) %2 == 0 ? "naine" : "mees");
+		}
+	public String aastakakstahte(){
+		return String.valueOf(18+(Integer.parseInt(kood.substring(0, 1))-1)/2);
+	}
+	public String synniaasta(){
+		return kood.substring(1,3);
+	}
+}
---
+++ 02_klass/Proovi1.java
@@ -0,0 +1,6 @@
+public class Proovi1{
+	public static void main(String[] arg){
+		Isikukood jaagup=new Isikukood("37605030299");
+		System.out.println(jaagup.sugu());
+	}
+}
