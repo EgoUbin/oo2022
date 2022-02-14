c6afdbf42cd7068c73ab8155ac5dc477417670ba
Egon Ubina <egubin@tlu.ee>
2022-02-14 08:18:45 +0200

    Update Kodu1.java

--- 01_funktsioon/Kodu/Kodu1.java
+++ 01_funktsioon/Kodu/Kodu1.java
@@ -1,10 +1,13 @@
 public class Kodu1{
-	public static void main(String[] arg){
-		int arv1=Integer.parseInt(arg[0]);
-		int arv2=Integer.parseInt(arg[1]);
+	public static sugu(String[] arg){
+		int alco=Integer.parseInt(arg[0]);
+		int kehamass=Integer.parseInt(arg[1]);
 		System.out.println(arv1*arv2);
 		for(int i=0; i<arv2; i++){
 			System.out.println("Tere.");
 		}
+	}
+	public static void alco((String[] arg)){
+		
 	}
 }
