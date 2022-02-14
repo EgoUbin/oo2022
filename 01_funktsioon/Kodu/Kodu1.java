7e391c56f57d3bdddb1263d72c4bcf44946207e0
Egon Ubina <egubin@tlu.ee>
2022-02-14 11:35:35 +0200

    Update Kodu1.java

--- 01_funktsioon/Kodu/Kodu1.java
+++ 01_funktsioon/Kodu/Kodu1.java
@@ -2,15 +2,15 @@ import java.util.Scanner;
 
 public class Kodu1{
 	public static char kook(){
-		Scanner sisestus = new Scanner(System.in);
+		Scanner sisestus = new Scanner(System.in); // Valmistab andmeid võtma.
 		System.out.println("Kas tahad kooki? Y/N");
-		char test = sisestus.next().charAt(0);
+		char test = sisestus.next().charAt(0); //muudab sisestatud tähe char formaati.
 		return test;
 	}
 	
 	public static void main(String[] args){ 
-		char vastus = kook();
-		if (vastus == 'Y'){
+		char vastus = kook(); //Kutsub funktsiooni.
+		if (vastus == 'Y'){  //If funktsioon, et andmetega midagi teha.
 			System.out.println("Palun. S66 kooki.");
 		}
 		else{
