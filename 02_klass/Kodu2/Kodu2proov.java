08c26ae23148cb13a25660fa63ddb984e230fc10
Egon Ubina <egubin@tlu.ee>
2022-02-21 10:18:37 +0200

    Update Kodu2proov.java

--- 02_klass/Kodu2/Kodu2proov.java
+++ 02_klass/Kodu2/Kodu2proov.java
@@ -1,10 +1,6 @@
 public class Proov2{
 	public static void main(String[] arg){
 		Kilpkonn k1=new Kilpkonn();
-		//k1.edasi();
-		//k1.keera();
-		//k1.edasi();
-		//k1.keera();
 		
 		for(int i=0; i<arg.length; i++){
 			if(arg[i].equals("e")){k1.edasi();}
