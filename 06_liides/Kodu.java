f99977d7095334b3d259dd22b3a5e2b298aba7e2
Egon Ubina <egubin@tlu.ee>
2022-03-21 11:59:30 +0200

    Uus kodutöö

--- 06_liides/Kodu.java
+++ 06_liides/Kodu.java
@@ -1,25 +1,43 @@
 public class Kodu{
 	
-	public static interface Transport{
-		public void kiirus();
-		public void maht();
+	public static interface Kujund{
+		double getRuumala();
+		double getPindala();
 	}
 	
-	static class Auto implements Transport{
-		public void kiirus(){System.out.println("Auto kiirus on 20 km/h");}
-		public void maht(){System.out.println("Auto mahutab 5 inimest");}
+	static class RistTahukas implements Kujund{
+		double pikkus;
+		double korgus;
+		double laius;
+		double pindala;
+		double ruumala;
+		public double getPindala(){pindala = ((pikkus * korgus) + (pikkus * laius) + (laius * korgus))*2; return pindala;}
+		public double getRuumala(){ruumala = pikkus * laius * korgus; return ruumala;}
+		
 	}
-	static class Kivi implements Transport{
-		public void kiirus(){System.out.println("Kivi kiirus on 0 km/h");}
-		public void maht(){System.out.println("Kivi mahutab 1-e inimese");}
+	static class Kera implements Kujund{
+		double raadius;
+		double pindala;
+		double ruumala;
+		public double getPindala(){pindala = 4 * 3.14 * (raadius * raadius); return pindala;}
+		public double getRuumala(){ruumala = (4/3) * 3.14 * (raadius * raadius * raadius); return ruumala;}
 	}
 	
 	public static void main(String []args){
-		Auto auto1 = new Auto();
-		Kivi kivi1 = new Kivi();
-		auto1.kiirus();
-		auto1.maht();
-		kivi1.kiirus();
-		kivi1.maht();
+		RistTahukas kujund1 = new RistTahukas();
+		Kera kujund2 = new Kera();
+		kujund1.pikkus = 10;
+		kujund1.laius = 20;
+		kujund1.korgus = 2;
+		kujund1.getPindala();
+		System.out.println("Kujund 1 pindala on " + kujund1.pindala);
+		kujund1.getRuumala();
+		System.out.println("Kujund 1 ruumala on " + kujund1.ruumala);
+		
+		kujund2.raadius = 12;
+		kujund2.getPindala();
+		System.out.println("Kujund 2 pindala on " + kujund2.pindala);
+		kujund2.getRuumala();
+		System.out.println("Kujund 2 ruumala on " + kujund2.ruumala);
 	}
 }
